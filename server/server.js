5aa345bdf87cf0ee0a2d6286e9e1e813c7021e14
Arthur-Oluoch-KE <arthuroluoch624@gmail.com>
2025-07-29 17:13:50 +0300

    Configuring CORS

--- server/server.js
+++ server/server.js
@@ -9,8 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 3000; 
 
 // Middleware
-app.use(cors());
+
 app.use(express.json());
+
+app.use(cors({
+  origin: 'https://edubot-eight.vercel.app',   
+  credentials: true,
+}))
 
 // Mathstral-7B API call
 async function askMathstral(question) {
